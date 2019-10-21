5dfe7baeb282a8425fb2691fa457f0f10f1cb252
Lmm-githup <18701623984@163.com>
2019-10-21 10:20:52 +0800

    第二版本和后台正合过的

--- json/score.js
+++ json/score.js
@@ -1,14 +1,14 @@
 var arr = [
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"50",type:"行为积分"},
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"40",type:"消费积分"},
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"80",type:"消费积分"},
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"70",type:"行为积分"},
-          ]
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1000, marketprice:"50", excPointType:"行为积分"},
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:800, marketprice:"40", excPointType:"消费积分"},
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1600, marketprice:"80", excPointType:"消费积分"},
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1400, marketprice:"70", excPointType:"行为积分"},
+]
 var arr1 = [
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"50",type:"消费积分"},
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"60",type:"消费积分"},
-          ]
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:800, marketprice:"40", excPointType:"消费积分"},
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1600, marketprice:"80", excPointType:"消费积分"},
+]
 var arr2 = [
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"50",type:"行为积分"},
-           {img:"images/lun.png",name:"我是一个商品名字",score:"2000",price:"60",type:"行为积分"},
-          ]
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1000, marketprice:"50", excPointType:"行为积分"},
+	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1400, marketprice:"70", excPointType:"行为积分"},
+]
