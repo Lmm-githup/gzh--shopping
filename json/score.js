98d4483ceef08adbee6d0a865790f7a1c54fecb4
Lmm-githup <18701623984@163.com>
2019-10-28 11:30:22 +0800

    修改了图标与商品名称太长多余变省略号

--- json/score.js
+++ json/score.js
@@ -1,5 +1,5 @@
 var arr = [
-	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1000, marketprice:"50", excPointType:"行为积分"},
+	{img:"images/轮播图.png",name:"我是一个商恒河沙数横说竖说时候时尚横说竖说活生生品名字", vNum:"1-4L40LL",excPoint:1000, marketprice:"50", excPointType:"行为积分"},
 	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:800, marketprice:"40", excPointType:"消费积分"},
 	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1600, marketprice:"80", excPointType:"消费积分"},
 	{img:"images/轮播图.png",name:"我是一个商品名字", vNum:"1-4L40LL",excPoint:1400, marketprice:"70", excPointType:"行为积分"},
